f04f4520431ab75f719f7c48b06a5ccfaec04687
Dawn Monroe <deetriplett@gmail.com>
2017-02-02 14:52:12 -0500

    added const response
    and module.exports

--- weather.js
+++ weather.js
@@ -4,4 +4,23 @@ const api = request('./api.json');
 //Print Out temp Details
 //Print out error message
 
-function get(query) {};
+function get(query) {
+const request = 
+    https.get('https://api.underground.com/api/${api.key}/geolookup/conditions/q/${query}.json', response => {
+      let body = "";
+      //Read data
+      response.on('data', chunk => {
+        body += chunk;
+      });
+      response.on('end', () => {
+        console.log(body);
+        //Parse data
+        //Print data
+      });
+     });//End Response
+           
+ }//End get(query) function
+        
+module.exports.get = get;
+        
+//TODO; Handle any errors
